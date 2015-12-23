c484590652e8a61ccf760f5581b7ad16fa5cded9
zacharysimon <zps1009@gmail.com>
2015-12-22 19:05:26 -0600

    styled navbar

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,18 @@ class Listing < ActiveRecord::Base
 
   belongs_to :user
   has_many :comments 
+
+
+  def full_address
+    city 
+  end
+
+
+  def cost_per_sqft
+    if price && sqft 
+      return price / sqft 
+    else
+      return "Need more info"
+    end
+  end
 end
