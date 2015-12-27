c785cdcd0647ed852578e8fd0ea830b8a7eb578b
zacharysimon <zps1009@gmail.com>
2015-12-27 15:28:05 -0600

    added new create page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,56 +4,57 @@ class ListingsController < ApplicationController
    
 
   def index
-    sort_attribute = params[:input_sort]
-    @sort_order = params[:input_sort_order]
 
-    if params[:input_sort_order] == "asc"
-      @new_sort_order = "desc"
-    else
-      @new_sort_order = "asc"
-    end
+    #this code is what makes me able to sort based on certain values, will probably replace this with JS soon
+    # sort_attribute = params[:input_sort]
+    # @sort_order = params[:input_sort_order]
 
-     if params[:input_sort] && params[:input_sort_order]
-      @listings = current_user.listings.order(sort_attribute => @sort_order)
-     else
-      @listings = current_user.listings.all
-     end
+    # if params[:input_sort_order] == "asc"
+    #   @new_sort_order = "desc"
+    # else
+    #   @new_sort_order = "asc"
+    # end
 
-  end
+    #  if params[:input_sort] && params[:input_sort_order]
+    #   @listings = current_user.listings.order(sort_attribute => @sort_order)
+    #  else
+    #   @listings = current_user.listings.all
+    #  end
 
-  def create
-    response = zillow_get_deep_search_results(params)
-
-    p response 
-
-    Listing.create(response)
-
-    @listing = Listing.new(
-      user_id: current_user.id,
-      address: response[:address],
-      city: response[:city],
-      state: response[:state],
-      latitude: response[:latitude],
-      longitude: response[:longitude],
-      price: response[:price],
-      zpid: response[:zpid],
-      zip_code: response[:zipcode],
-      bathrooms: response[:bathrooms],
-      bedrooms: response[:bedrooms],
-      sqft: response[:sqft],
-      home_type: response[:hometype],
-      )
-
-    if @listing.save 
-      flash[:success] = "Listing successfully created!"
-      redirect_to '/listings'
-    end
+    @listings = current_user.listings.all
 
   end
 
   def new
     @listing = Listing.new
   end
+
+  def create
+
+    inputs = defaults(params)
+
+    @listing = Listing.new(
+      user_id: current_user.id,
+      address: inputs[:address],
+      city: inputs[:city],
+      state: inputs[:state],
+      latitude: inputs[:latitude],
+      longitude: inputs[:longitude],
+      price: inputs[:price],
+      zpid: inputs[:zpid],
+      zip_code: inputs[:zipcode],
+      bathrooms: inputs[:bathrooms],
+      bedrooms: inputs[:bedrooms],
+      sqft: inputs[:sqft],
+      )
+
+    if @listing.save 
+      flash[:success] = "Listing successfully created!"
+      redirect_to "/listings/#{@listing.id}/edit"
+    end
+
+  end
+
 
   def show
     @listing = Listing.find_by(id: params[:id])
--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -8,19 +8,19 @@ module ListingsHelper
 
     response = HTTParty.get("http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz19ytk7im2ob_728x4&address=#{input_street_address}&citystatezip=#{input_city},#{input_state}")
 
-    zpid = response["searchresults"]["response"]["results"]["result"]["zpid"]
-    latitude = response["searchresults"]["response"]["results"]["result"]["address"]["latitude"]
-    longitude = response["searchresults"]["response"]["results"]["result"]["address"]["longitude"]
-    price = response["searchresults"]["response"]["results"]["result"]["zestimate"]["amount"]["__content__"]
-    address = response["searchresults"]["response"]["results"]["result"]["address"]["street"]
-    city = response["searchresults"]["response"]["results"]["result"]["address"]["city"]
-    zipcode = response["searchresults"]["response"]["results"]["result"]["address"]["zipcode"]
-    state = response["searchresults"]["response"]["results"]["result"]["address"]["state"]
-    bathrooms = response["searchresults"]["response"]["results"]["result"]["bathrooms"]
-    bedrooms = response["searchresults"]["response"]["results"]["result"]["bedrooms"]
-    sqft = response["searchresults"]["response"]["results"]["result"]["finishedSqFt"]
-    hometype = response["searchresults"]["response"]["results"]["result"]["useCode"]
-
+    if response.parsed_response["searchresults"]["message"]["code"] != "508"
+        zpid = response["searchresults"]["response"]["results"]["result"]["zpid"]
+        latitude = response["searchresults"]["response"]["results"]["result"]["address"]["latitude"]
+        longitude = response["searchresults"]["response"]["results"]["result"]["address"]["longitude"]
+        price = response["searchresults"]["response"]["results"]["result"]["zestimate"]["amount"]["__content__"]
+        address = response["searchresults"]["response"]["results"]["result"]["address"]["street"]
+        city = response["searchresults"]["response"]["results"]["result"]["address"]["city"]
+        zipcode = response["searchresults"]["response"]["results"]["result"]["address"]["zipcode"]
+        state = response["searchresults"]["response"]["results"]["result"]["address"]["state"]
+        bathrooms = response["searchresults"]["response"]["results"]["result"]["bathrooms"]
+        bedrooms = response["searchresults"]["response"]["results"]["result"]["bedrooms"]
+        sqft = response["searchresults"]["response"]["results"]["result"]["finishedSqFt"]
+    end
 
   {
     zpid: zpid,
@@ -37,7 +37,79 @@ module ListingsHelper
   }
   end
 
-  
+  def defaults(params)
+    zillow = zillow_get_deep_search_results(params)
+
+    if zillow["zpid"] == nil
+        zpid = 0000
+    else zpid = zillow["zpid"]
+    end
+
+    if zillow["latitude"] == nil
+        latitude = 0000
+    else latitude = zillow["latitude"]    
+    end
+
+    if zillow["longitude"] == nil
+        longitude = 0000
+    else longitude = zillow["longitude"]
+    end 
+
+    if zillow["address"] == nil
+        address = params[:address].to_s
+    else address = zillow["address"]
+    end
+
+    if zillow["city"] == nil
+        city = params[:city].to_s
+    else city = zillow["city"]   
+    end
+
+    if zillow["state"] == nil
+        state = params[:state].to_s
+    else state = zillow["state"]
+    end 
+
+    if zillow["bedrooms"] == nil
+        bedrooms = 0
+    else bedrooms = zillow["bedrooms"]
+    end 
+
+    if zillow["bathrooms"] == nil
+        bathrooms = 0
+    else bathrooms = zillow["bathrooms"]
+    end
+
+    if zillow["sqft"] == nil
+        sqft = 0
+    else sqft = zillow["sqft"]    
+    end
+
+    if zillow["zipcode"] == nil
+        zipcode = 00000
+    else zipcode = zillow["zipcode"]
+    end 
+    if zillow["price"] == nil
+        price = params[:price]
+    else price = zillow["price"]
+    end 
+
+    {
+    zpid: zpid,
+    latitude: latitude,
+    longitude: longitude,
+    price: price,
+    address: address,
+    city: city,
+    zipcode: zipcode,
+    state: state,
+    bathrooms: bathrooms,
+    bedrooms: bedrooms,
+    sqft: sqft,
+  }
+
+
+  end
 
    
 
