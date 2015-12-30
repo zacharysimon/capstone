d026a21d1c7731a87290a50e2c4e8391d4bac760
zacharysimon <zps1009@gmail.com>
2015-12-29 18:05:53 -0600

    styled the edit show page

--- app/controllers/assumptions_controller.rb
+++ app/controllers/assumptions_controller.rb
@@ -13,7 +13,6 @@ class AssumptionsController < ApplicationController
   def update
     current_user.update(
       percent_down_pmt: params[:input_percent_down_pmt],
-      credit_score_bucket: params[:input_credit_score_bucket],
       loan_type: params[:input_loan_type],
       acquisition_costs: params[:input_acquisition_costs],
       disposition_costs: params[:input_disposition_costs],
@@ -22,7 +21,6 @@ class AssumptionsController < ApplicationController
       rent_growth: params[:input_rent_growth],
       property_appreciation: params[:input_property_appreciation],
       income_tax_rate: params[:input_income_tax_rate],
-      income: params[:input_income]
       )
 
     flash[:success] = "Profile was successfully updated!"
