6a63ce6af8a25fc83763657d12968b5ea7a64a83
zacharysimon <zps1009@gmail.com>
2016-03-28 13:00:27 -0500

    fix comment error by changing comment_type to type

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
       user_id: current_user.id,
       listing_id: params[:input_listing_id],
       comment: params[:input_comment],
-      comment_type: params[:input_comment_type],
+      #error here before with comment_type vs. type database attribute
+      type: params[:input_comment_type],
       score: params[:input_score]
       )
     if comment.save
